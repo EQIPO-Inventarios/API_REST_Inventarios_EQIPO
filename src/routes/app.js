78b6318403e80ac0dc189cd05ba3fdd5c738878d
Jhosep Islam <jhosepchai@gmail.com>
2020-09-09 17:19:43 -0600

    ruta de prueba

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,18 +1,37 @@
 const express = require("express"),
 app = express.Router();
 
-const Usuarios = require("../controllers/Usuarios")
+const Usuario = require("../models/Usuario")
 
 var swaggerUi = require('swagger-ui-express'),
     swaggerDocument = require("./swagger/swagger.json");
 
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
+/*
 app.all("/test", (req, res)=>{
     res.send("hola mundo");
 })
-app.get("/listUsuarios",Usuarios.listarUsuarios)
-app.post("/login",Usuarios.login)
+app.get("/Usuarios/listar",Usuarios.listarUsuarios)
+app.post("/Usuarios/login",Usuarios.login)
+*/
+app.post("/Usuarios/new", (req, res) =>{
+    try {
+        const user = new Usuario(req.body)
+         user.save()
+        const token =  user.generateAuthToken()
+        res.status(201).send({ user, token })
+     } catch (error) {
+        res.status(400).send(error)
+     }
+})
+
+
+app.get("/prueba",(req, res)=>{
+    res.status(200).json({
+        saludo : "hola mundo",
+        creado : "mi v:"
+    })
+})
 
 module.exports = app;
