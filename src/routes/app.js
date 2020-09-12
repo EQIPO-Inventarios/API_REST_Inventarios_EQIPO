9e1fe975b7bae9e8849b2060504e9f7171ade567
Jhosep Islam <jhosepchai@gmail.com>
2020-09-12 16:05:39 -0600

    Cambios en fichero de modelos y rutas

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,7 +1,7 @@
 const express = require("express"),
 app = express.Router();
 
-const Usuario = require("../models/Usuario")
+const Usuarios = require("../controllers/Usuarios")
 
 var swaggerUi = require('swagger-ui-express'),
     swaggerDocument = require("./swagger/swagger.json");
@@ -15,16 +15,11 @@ app.all("/test", (req, res)=>{
 app.get("/Usuarios/listar",Usuarios.listarUsuarios)
 app.post("/Usuarios/login",Usuarios.login)
 */
-app.post("/Usuarios/new", (req, res) =>{
-    try {
-        const user = new Usuario(req.body)
-         user.save()
-        const token =  user.generateAuthToken()
-        res.status(201).send({ user, token })
-     } catch (error) {
-        res.status(400).send(error)
-     }
-})
+app.get("/Usuarios/listar",Usuarios.listarUsuarios)
+app.post("/Usuarios/login", Usuarios.login)
+
+
+
 
 
 app.get("/prueba",(req, res)=>{
