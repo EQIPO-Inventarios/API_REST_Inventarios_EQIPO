c3e5d066a66073ad547ad67dc0dff241393efa29
Jhosep Islam <jhosepchai@gmail.com>
2020-09-16 14:59:39 -0600

    Configuracion del fichero db,js
    Cambio en la url de bd, la bd ahora esta alojada en un servidor online

--- src/settings/db.js
+++ src/settings/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-
- mongoose.connect("mongodb://localhost/db_multibodegas",{
-    useNewUrlParser : true,
+//'mongodb://localhost/db_multibodegas'
+const conection_url ="mongodb+srv://islam:tjysCX2czL0vdZcD@cluster0.hgpt5.mongodb.net/db_api_mutlibodegas?retryWrites=true&w=majority";
+mongoose.connect(conection_url,{
+    useCreateIndex : true,
+    useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(data => console.log("db is connected"))
+.then(data => console.info("db is connected"))
 .then(err => console.error(err));
