4d2bd93794f7791b5ae91986d26ebca3bbd6b815
Jhosep Islam <jhosepchai@gmail.com>
2020-09-28 17:21:39 -0600

    Seguridad  Tokens
    Creados metodos para asignacion y validacion de token por 24h

---
+++ src/middleware/tokenValidator.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const rutasProtegidas = express.Router(); 
+
+rutasProtegidas.use((req, res, next) => {
+    const token = req.headers['access-token'];
+ 
+    if (token) {
+      jwt.verify(token, String(rutasProtegidas.get('llave')), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token inválida', err });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token no proveída.' 
+      });
+    }
+ });
+
+
+ const generateToken = (usuario) =>{
+    const payload = {
+        check:  true,
+        user : usuario
+       };
+       const token = jwt.sign(payload, String(rutasProtegidas.get('key')), {
+        expiresIn: 1440 //24horas
+       });       
+
+       return  token
+       
+ }
+
+ module.exports = {rutasProtegidas, generateToken}
