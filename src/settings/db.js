306cff0f76c650981d1c977801b81b83f97263e2
Jhosep Islam <jhosepchai@gmail.com>
2020-11-22 12:51:06 -0600

    Correccion error mongo
    correccion en cadena de conexion de mongo

--- src/settings/db.js
+++ src/settings/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //'mongodb://localhost/db_multibodegas'
 //"mongodb+srv://islam:tjysCX2czL0vdZcD@cluster0.hgpt5.mongodb.net/db_api_mutlibodegas?retryWrites=true&w=majority";
-const conection_url ="mongodb+srv://islam:tjysCX2czL0vdZcD@cluster0.hgpt5.mongodb.net/db_api_mutlibodegas?retryWrites=true&w=majority";
+const conection_url ="mongodb+srv://islam:TnbSoQKV9jH7WEXB@cluster0.hgpt5.mongodb.net/db_api_mutlibodegas?retryWrites=true&w=majority";
 mongoose.connect(conection_url,{
     useCreateIndex : true,
     useNewUrlParser: true,
